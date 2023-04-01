20ef604d4249c9a66c6d8659a407384a36ad2eed
Filip Jirsák <filip@jirsak.org>
2023-04-01 10:28:47 +0200

    feat: Autostart VS Code with created project

--- README.md
+++ README.md
@@ -15,3 +15,29 @@ If you want to have a vanilla JavaScript application without React use:
 ```bash
 npm init kodim-app my-app vanilla
 ```
+
+If you want to create an application in current directory, ude `.` as application name:
+
+```bash
+npm init kodim-app .
+npm init kodim-app . vanilla
+npm init kodim-app . html-css
+npm init kodim-app . html-js
+npm init kodim-app . html-css-js
+```
+
+
+## Disable VS Code autostart
+
+Use `--no-vscode` parameter, for example:
+
+```bash
+npm init kodim-app --no-vscode my-app
+npm init kodim-app --no-vscode my-app vanilla
+```
+
+## Show all parameters
+
+```bash
+npm init kodim-app -- --help
+```
--- index.js
+++ index.js
@@ -30,7 +30,13 @@ const argv = yargs(process.argv.slice(2))
         default: DEFAULT_SERVER_URL,
         describe: 'URL from where to fetch the kit files',
         type: 'string'
-    })
+      })
+      .option('c', {
+        alias: 'vscode',
+        default: true,
+        describe: 'Open created project in VS Code',
+        type: 'boolean'
+      })
   }).help().argv;
 
 (async () => {
@@ -64,6 +70,11 @@ const argv = yargs(process.argv.slice(2))
     }
 
     console.info(chalk.green(`Project '${argv.app_name}' created successfully.`));
+
+    if (argv.vscode) {
+      console.info(chalk.green(`Opening '${argv.app_name}' in VS Code.`));
+      spawnSync('code', ['.'], { cwd: result.appRoot, stdio: 'inherit' })
+    }
   } catch (error) {
     console.error(chalk.redBright(`ERROR: ${error.message}`));
   }
